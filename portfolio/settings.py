e7d5c3c8118e7a8a8fd77c4a562d341cff78ea20
Mohamed Sameh <m.samehmohamed123@gmail.com>
2020-07-21 18:51:24 +0200

    add local settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -125,3 +125,8 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+try:
+    from .local_settings import *
+except ImportError:
+    print('You are in Production Man!')
